d34e6267cdab77638e0502415fdab8813225dfab
pativashvili <gpativashvili@gmail.com>
2024-07-14 16:54:40 +0400

    test-deploy

--- students-segment-designer/enviroment/enviroment.ts
+++ students-segment-designer/enviroment/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   lecturerInfoApi: 'https://studentsegmentdesigner.azurewebsites.net',
-  fireBaseCofnig : {
+  fireBaseCofnig: {
     apiKey: 'AIzaSyBYRREjibT3kjLW8vs2z2ZVvtz34GRTP3U',
     authDomain: 'students-segment-designer.firebaseapp.com',
     projectId: 'students-segment-designer',
@@ -9,6 +9,6 @@ export const environment = {
     messagingSenderId: '492237126831',
     appId: '1:492237126831:web:805fc7e89d7ea31614a85a',
     measurementId: 'G-MVF2KXB4JR',
-  }
+  },
   // Other environment variables
 };
